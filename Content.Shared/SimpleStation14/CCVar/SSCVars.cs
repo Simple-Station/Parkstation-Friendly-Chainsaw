e47af8406c3cb3b85a66e67ed8e691bd462c1033
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-11-03 21:17:49 -0700

    fix CVar names

--- Content.Shared/SimpleStation14/CCVar/SSCVars.cs
+++ Content.Shared/SimpleStation14/CCVar/SSCVars.cs
@@ -9,11 +9,11 @@ public sealed class SimpleStationCCVars
     ///     Optionally force set an announcer
     /// </summary>
     public static readonly CVarDef<string> Announcer =
-        CVarDef.Create("game.announcer", "random", CVar.SERVERONLY);
+        CVarDef.Create("announcer.announcer", "random", CVar.SERVERONLY);
 
     /// <summary>
     ///     Optionally blacklist announcers
     /// </summary>
     public static readonly CVarDef<List<string>> AnnouncerBlacklist =
-        CVarDef.Create("game.announcer.blacklist", new List<string>(), CVar.SERVERONLY);
+        CVarDef.Create("announcer.blacklist", new List<string>(), CVar.SERVERONLY);
 }
