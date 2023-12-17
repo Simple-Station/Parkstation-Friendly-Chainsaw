af3499f00b897d15f27a945804231368a3a2ae40
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-12-16 23:43:44 -0800

    revert to commit "fffff"

--- Content.Server/SimpleStation14/Weapons/Ranged/Components/RandomFireGunOnDropComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server.SimpleStation14.Weapons.Ranged.Components;
-
-[RegisterComponent]
-public sealed partial class RandomFireGunOnDropComponent : Component
-{
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public float FireOnDropChance = 0.1f;
-}
