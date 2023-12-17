b44af6110bcd6708e242f29943801b3d91f00431
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-12-16 23:23:16 -0800

    remove component DataField name specifiers and make every variable VVRW

--- Content.Server/SimpleStation14/Speech/Components/RandomBarkComponent.cs
+++ Content.Server/SimpleStation14/Speech/Components/RandomBarkComponent.cs
@@ -7,27 +7,27 @@ namespace Content.Server.SimpleStation14.Speech.Components;
 public sealed partial class RandomBarkComponent : Component
 {
     // Should the message be sent to the chat log?
-    [DataField("chatlog")]
-    public bool Chatlog = false;
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool ChatLog = false;
 
     // Minimum time an animal will go without speaking
-    [DataField("minTime")]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public int MinTime = 45;
 
     // Maximum time an animal will go without speaking
-    [DataField("maxTime")]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public int MaxTime = 350;
 
     // Counter
-    [DataField("barkAccumulator")]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float BarkAccumulator = 8f;
 
     // Multiplier applied to the random time. Good for changing the frequency without having to specify exact values
-    [DataField("barkMultiplier")]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public float BarkMultiplier = 1f;
 
     // List of things to be said. Filled with garbage to be modified by an accent, but can be specified in the .yml
-    [DataField("barks"), ViewVariables(VVAccess.ReadWrite)]
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
     public IReadOnlyList<string> Barks = new[]
     {
         "Bark",
