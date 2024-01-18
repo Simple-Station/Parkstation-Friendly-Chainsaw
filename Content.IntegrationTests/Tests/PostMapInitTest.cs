cc2f5f8cab250bb889c8bc35084bd12ef3cb9b20
Adrian16199 <piaskowskiadrian0@gmail.com>
2024-01-18 20:02:08 +0100

    Sorting departments.
    TO-DO:
    -Get rid of certain syndicate objectives
    -Find out how to get rid of Logistics in department.yml
    -Add service supervisor from parkstation
    -probably adjust icons to what they were in parkstation
    -get rid of shoukou when its time to actualy get this on review.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -64,7 +64,7 @@ namespace Content.IntegrationTests.Tests
             "MeteorArena",
             // "Pebble", //DeltaV
             // "Edge", //DeltaV
-            // "Shoukou", //DeltaV
+             "Shoukou", //DeltaV
             // "Tortuga", //DeltaV
             // "Arena", //DeltaV
             // "Asterisk", //DeltaV
