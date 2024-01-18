45bae9c7162bb85e965fcb6b34703cf846d5a5ed
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-01-17 23:53:43 -0800

    split tests and integration tests scripts into two

--- Scripts/bat/runTestsIntegration.bat
+++ Scripts/bat/runTestsIntegration.bat
@@ -5,9 +5,6 @@ dotnet build --configuration DebugOpt --no-restore /p:WarningsAsErrors=nullable 
 
 mkdir Scripts\logs
 
-del Scripts\logs\Content.Tests.log
-dotnet test --no-build --configuration DebugOpt Content.Tests/Content.Tests.csproj -- NUnit.ConsoleOut=0 > Scripts\logs\Content.Tests.log
-
 del Scripts\logs\Content.IntegrationTests.log
 dotnet test --no-build --configuration DebugOpt Content.IntegrationTests/Content.IntegrationTests.csproj -- NUnit.ConsoleOut=0 NUnit.MapWarningTo=Failed > Scripts\logs\Content.IntegrationTests.log
 
--- Scripts/sh/runTests.sh
+++ Scripts/sh/runTests.sh
@@ -5,8 +5,8 @@ dotnet build --configuration DebugOpt --no-restore /p:WarningsAsErrors=nullable 
 
 mkdir Scripts/logs
 
-rm Scripts/logs/Content.YAMLLinter.log
-dotnet run --project Content.YAMLLinter/Content.YAMLLinter.csproj --no-build -- NUnit.ConsoleOut=0 > Scripts/logs/Content.YAMLLinter.log
+rm Scripts/logs/Content.Tests.log
+dotnet test --no-build --configuration DebugOpt Content.Tests/Content.Tests.csproj -- NUnit.ConsoleOut=0 > Scripts/logs/Content.Tests.log
 
 echo "Tests complete. Press enter to continue."
 read
