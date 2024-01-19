39ac47f9c3aebc7702f9c27468d8da4ddf28ce68
Finket <va.ketola@gmail.com>
2024-01-19 19:08:58 +0200

    Flatten nested UI in cs

--- Content.Client/CosmaticDrift/CryoSleep/AcceptCryoWindow.cs
+++ Content.Client/CosmaticDrift/CryoSleep/AcceptCryoWindow.cs
@@ -8,51 +8,33 @@ public sealed class AcceptCryoWindow : DefaultWindow
 {
     public readonly Button DenyButton;
     public readonly Button AcceptButton;
+    public readonly BoxContainer ButtonBox;
+    public readonly BoxContainer InnerBox;
 
     public AcceptCryoWindow()
     {
 
         Title = Loc.GetString("accept-cryo-window-title");
 
-        Contents.AddChild(new BoxContainer
+        AcceptButton = new Button() {Text = Loc.GetString("accept-cryo-window-accept-button")};
+        DenyButton = new Button() { Text = Loc.GetString("accept-cryo-window-deny-button")};
+
+        // This one holds the buttons
+        ButtonBox = new BoxContainer()
+        {
+            Orientation = LayoutOrientation.Horizontal,
+            Align = AlignMode.Center,
+            Children = {AcceptButton, (new Control() { MinSize = new Vector2i(20, 0) }), DenyButton}
+        };
+
+        // This one holds the button container
+        InnerBox = new BoxContainer()
         {
             Orientation = LayoutOrientation.Vertical,
-            Children =
-            {
-                new BoxContainer
-                {
-                    Orientation = LayoutOrientation.Vertical,
-                    Children =
-                    {
-                        (new Label()
-                        {
-                            Text = Loc.GetString("accept-cryo-window-prompt-text-part")
-                        }),
-                        new BoxContainer
-                        {
-                            Orientation = LayoutOrientation.Horizontal,
-                            Align = AlignMode.Center,
-                            Children =
-                            {
-                                (AcceptButton = new Button
-                                {
-                                    Text = Loc.GetString("accept-cryo-window-accept-button"),
-                                }),
+            Children = {(new Label() {Text = Loc.GetString("accept-cryo-window-prompt-text-part")}), ButtonBox}
+        };
 
-                                (new Control()
-                                {
-                                    MinSize = new Vector2i(20, 0)
-                                }),
-
-                                (DenyButton = new Button
-                                {
-                                    Text = Loc.GetString("accept-cryo-window-deny-button"),
-                                })
-                            }
-                        },
-                    }
-                },
-            }
-        });
+        // Put it all together
+        Contents.AddChild(new BoxContainer { Orientation = LayoutOrientation.Vertical, Children = { InnerBox } });
     }
 }
