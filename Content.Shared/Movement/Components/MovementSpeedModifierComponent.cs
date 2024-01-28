28cd3befd97d207f2755db19534ecc61597575c5
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-01-27 23:34:16 -0800

    make mobs have to accelerate a bit to move or stop

--- Content.Shared/Movement/Components/MovementSpeedModifierComponent.cs
+++ Content.Shared/Movement/Components/MovementSpeedModifierComponent.cs
@@ -18,9 +18,11 @@ namespace Content.Shared.Movement.Components
         public const float DefaultWeightlessModifier = 0.7f;
         public const float DefaultWeightlessAcceleration = 1f;
 
-        public const float DefaultAcceleration = 20f;
-        public const float DefaultFriction = 20f;
-        public const float DefaultFrictionNoInput = 20f;
+        // Parkstation-AcceleratingMovement Start
+        public const float DefaultAcceleration = 8f;
+        public const float DefaultFriction = 8f;
+        public const float DefaultFrictionNoInput = 8f;
+        // Parkstation-AcceleratingMovement End
 
         public const float DefaultBaseWalkSpeed = 3f;
         public const float DefaultBaseSprintSpeed = 5f;
