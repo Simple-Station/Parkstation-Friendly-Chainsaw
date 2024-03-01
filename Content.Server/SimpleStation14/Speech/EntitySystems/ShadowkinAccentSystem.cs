58e1f2320828aa3da7fd60d79648fbec7d79e2a3
Finket <va.ketola@gmail.com>
2024-03-01 13:30:35 +0200

    Fix shadowkin accent not capitalizing first letters

--- Content.Server/SimpleStation14/Speech/EntitySystems/ShadowkinAccentSystem.cs
+++ Content.Server/SimpleStation14/Speech/EntitySystems/ShadowkinAccentSystem.cs
@@ -9,9 +9,12 @@ namespace Content.Server.SimpleStation14.Speech.EntitySystems
     {
         [Dependency] private readonly IRobustRandom _random = default!;
 
-        private static readonly Regex mRegex = new(@"[adgjmpsv]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        private static readonly Regex aRegex = new(@"[behknqtwy]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        private static readonly Regex rRegex = new(@"[cfiloruxz]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex mRegex = new(@"[adgjmpsv]", RegexOptions.Compiled);
+        private static readonly Regex aRegex = new(@"[behknqtwy]", RegexOptions.Compiled);
+        private static readonly Regex rRegex = new(@"[cfiloruxz]", RegexOptions.Compiled);
+        private static readonly Regex MRegex = new(@"[ADGJMPSV]", RegexOptions.Compiled);
+        private static readonly Regex ARegex = new(@"[BEHKNQTWY]", RegexOptions.Compiled);
+        private static readonly Regex RRegex = new(@"[CFILORUXZ]", RegexOptions.Compiled);
 
 
         public override void Initialize()
@@ -28,6 +31,9 @@ namespace Content.Server.SimpleStation14.Speech.EntitySystems
             message = mRegex.Replace(message, "m");
             message = aRegex.Replace(message, "a");
             message = rRegex.Replace(message, "r");
+            message = MRegex.Replace(message, "M");
+            message = ARegex.Replace(message, "A");
+            message = RRegex.Replace(message, "R");
 
             return message;
         }
